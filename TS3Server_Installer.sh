9f9d2bf25581ad1f4b534306866c46ac9acc32c9
Jakob434 <167235177+Jakob434@users.noreply.github.com>
2024-04-17 13:39:18 +0200

    Update TS3Server_Installer.sh

--- TS3Server_Installer.sh
+++ TS3Server_Installer.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
+TS3 Server aufsetzen
 
 adduser ts3
 
 su ts3
 
-cd
+cd 
 
 wget https://files.teamspeak-services.com/releases/server/3.13.7/teamspeak3-server_linux_amd64-3.13.7.tar.bz2
 
-tar xfjv teamspeaks3-server_linux_amd64-3.13.7.tar.bz2
+tar xfjv name… Tab für Auto Vervollständigung 
 
-cd teamspeaks3-server_linux_amd64
+cd teamspeak3-server_linux_amd64
+
+ls 
 
 ./ts3server_startscript.sh start
 
+touch .ts3server_license_accepted
 
 ./ts3server_startscript.sh start
